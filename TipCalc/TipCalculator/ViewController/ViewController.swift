b5da33c3367c6f6f61b521413e73b1d838948a14
Medi Assumani <mediassumani49@gmail.com>
2018-02-17 18:58:47 -0500

    adding logig

--- TipCalc/TipCalculator/ViewController/ViewController.swift
+++ TipCalc/TipCalculator/ViewController/ViewController.swift
@@ -19,11 +19,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipPercentSelector: UISegmentedControl!
     
     
-    
-    
-    
     // - MARK : IBACTIONS
     @IBAction func calculateButtonTaped(_ sender: Any) {
+        
+        
+                    //SAFELY UNWRAPING IBOULETS OPTIONALS
+        guard let bill = self.billAmountTextField.text, let tipAmount = self.tipAmountTextField.text, let total = self.totalAmountTextField.text else {return}
+        
+        if bill = self.billAmountTextField.text as? Double{
+            
+            var tipPercentage : Double = 0.0
+            
+            switch self.tipPercentSelector.selectedSegmentIndex {
+            case 0:
+                
+                tipPercentage = 5.0
+                
+            case 1:
+                
+                tipPercentage = 10.0
+                
+            case 2:
+                
+                tipPercentage = 20.0
+                
+            default:
+                
+                return nil
+            }
+            
+        }
         
         
     }
